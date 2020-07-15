15157fdc9433a3c4bccf6498392f26d986761f8a
gkaragoz <gkaragoz96@gmail.com>
2020-07-16 01:07:35 +0300

    Seperate prod and dev keys. Also implement authentication stuff completely.

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,15 @@ const keys = require("../config/keys")
 
 const User = mongoose.model('users')
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then((user) => done(null, user))
+})
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
@@ -20,10 +29,12 @@ passport.use(
             .then((existingUser) => {
                 if (existingUser) {
                     // we already have a record with the given profile ID
-                    
+                    done(null, existingUser)
                 } else {
                     // we don't have a user record with this ID, make a new record!
-                    new User({ googleId: profile.id }).save()
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user))
                 }
             })
     })
